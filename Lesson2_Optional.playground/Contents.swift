d2cb1debe0d4a0e9aad8b8c0719c8287a8fd2d5a
zhangkaizhou <zhangkaizhou@hotmail.com>
2017-05-23 22:49:56 +0800

    解析可选项的值

--- Lesson2_Optional.playground/Contents.swift
+++ Lesson2_Optional.playground/Contents.swift
@@ -3,3 +3,18 @@
 import UIKit
 
 var str = "Hello, playground"
+
+// Example 1
+var zee: Int?
+
+let strings = ["ABC", "123"]
+let randomIndex = Int(arc4random() % 2)
+let anotherString = strings[randomIndex]
+
+zee = Int(anotherString)
+
+if let zee = zee {
+    print(zee * 2)
+} else {
+    print("no value")
+}
